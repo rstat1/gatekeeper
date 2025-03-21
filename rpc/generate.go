8c85b9cd4e0208062327ceb6c9018795ed3bd52b
rstat1 <rstat1@alargerobot.dev>
2025-03-21 00:26:20 -0400

    Make a separate SDK branch that just includes the sdk go code until go 1.25 is released.

---
+++ rpc/generate.go
@@ -0,0 +1,5 @@
+package sdk
+
+//go:generate protoc -Iproto --go_out=. types.proto
+
+//go:generate protoc -Iproto --go-grpc_out=. --go_out=. --go_opt=Mtypes.proto=go.alargerobot.dev/gatekeeper/sdk/rpc/types --go-grpc_opt=Mtypes.proto=go.alargerobot.dev/gatekeeper/sdk/rpc/types EndpointManager.proto
