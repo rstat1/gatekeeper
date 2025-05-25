0d23a6158c59f6e0b04cec893768448c90f49e92
rstat1 <rstat1@alargerobot.dev>
2025-05-25 16:41:36 -0400

    Add helper process for EDCs that helps with credential renewal.

---
+++ src/edc_helper/go.mod
@@ -0,0 +1,17 @@
+module edc_helper
+
+go 1.24.1
+
+require github.com/sirupsen/logrus v1.9.3
+
+require (
+	github.com/fsnotify/fsnotify v1.9.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	go.alargerobot.dev/gatekeeper/sdk v0.0.0-20250520055450-fa71c30a1804 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250115164207-1a7da9e5054f // indirect
+	google.golang.org/grpc v1.71.0 // indirect
+	google.golang.org/protobuf v1.36.4 // indirect
+)
---
+++ src/edc_helper/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"flag"
+	"edc_helper/common"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"go.alargerobot.dev/gatekeeper/sdk"
+	v1 "go.alargerobot.dev/gatekeeper/sdk/rpc/config/v1"
+)
+
+type config struct {
+	ServerEndpoint string `json:"serverEndpoint"`
+}
+
+func main() {
+	var clientConfig config
+	common.CommonProcessInit(false, false)
+
+	svcName := flag.String("clientProcessName", "", "set to the name of the process connecting to gatekeeper. Cannot be null")
+	if *svcName == "" {
+		panic(errors.New("clientProcessName MUST BE SET"))
+	}
+
+	if _, e := os.Stat(*svcName); os.IsNotExist(e) {
+		panic(errors.New("clientProcessName MUST BE SET TO SOMETHING THAT ACTUALLY EXISTS"))
+	}
+
+	confFile, err := os.ReadFile("client_config.json")
+	if err != nil {
+		panic(err)
+	}
+	json.Unmarshal(confFile, &clientConfig)
+
+	edc := sdk.NewExternalDeviceClient(sdk.ExternalDeviceClientConfig{
+		ServiceURL:           clientConfig.ServerEndpoint,
+		EndpointServicesAddr: common.GetOutboundIP() + ":13337",
+		CertificateRenewalHandler: func(sc v1.ServiceCredentials) {
+			if e := os.WriteFile("gkroot.crt", []byte(sc.CaCert), 0o600); e != nil {
+				common.LogWarn("err", e, "failed to write new root")
+			}
+
+			if e := os.WriteFile(*svcName+".crt", []byte(sc.Certificate), 0o600); e != nil {
+				common.LogWarn("err", e, "failed to write new cert")
+			}
+
+			if e := os.WriteFile(*svcName+".key", []byte(sc.PrivateKey), 0o600); e != nil {
+				common.LogWarn("err", e, "failed to write new private key")
+			}
+		},
+	}, nil)
+
+	if err := edc.Login(); err != nil {
+		panic(err)
+	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+	<-c
+
+	os.Exit(0)
+}
